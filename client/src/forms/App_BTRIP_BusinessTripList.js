183ef17dc822247d48db1bc9aec4d0b126aa9804
Stepanskiy <64489503+Stepanskiy@users.noreply.github.com>
2025-08-24 11:30:18 +0300

    Add App_BTRIP_BusinessTripList component with initial structure

---
+++ client/src/forms/App_BTRIP_BusinessTripList.js
@@ -0,0 +1,14 @@
+/**
+ * @param {D5TableForm} form
+ * @param {D5Core} core
+ */
+export const App_BTRIP_BusinessTripList = (form, core) => ({
+  onInitForm() {
+
+  }
+});
+
+window.userScript = {
+  ...window.userScript,
+  App_BTRIP_BusinessTripList
+};

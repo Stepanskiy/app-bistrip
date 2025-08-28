bef243d91d27226e7a57e32f298487d95ffe21ec
Stepanskiy <64489503+Stepanskiy@users.noreply.github.com>
2025-08-28 13:46:07 +0300

    Enhance App_BTRIP_BusinessTripEdit and App_BTRIP_BusinessTripList components with initialization and filtering logic

--- client/src/forms/App_BTRIP_BusinessTripEdit.js
+++ client/src/forms/App_BTRIP_BusinessTripEdit.js
@@ -5,20 +5,24 @@ import { restoreFormParam } from "../lib/common";
  * @param {D5Core} core
  */
 export const App_BTRIP_BusinessTripEdit = (form, core) => ({
+  OnInit() {
+  },
   OnShow() {
     setFieldInitialValue(form);
-    console.log("### Hi there");
   }
 });
 
 function setFieldInitialValue(form) {
   let firmID = restoreFormParam("App_BTRIP_BusinessTripEdit", "FirmID");
-  if (form.field("FirmID").value = null) {
+  if (form.field("ID").value == null) {
     if (firmID instanceof Array) {
         form.field("FirmID").value = firmID[0];
     } else {
         form.field("FirmID").value = firmID;
     }
+    form.field("PersonID").filter = {"FirmID": firmID};
+  } else {
+    form.field("PersonID").filter = {"FirmID": -1};
   }
 }
 
--- client/src/forms/App_BTRIP_BusinessTripList.js
+++ client/src/forms/App_BTRIP_BusinessTripList.js
@@ -5,13 +5,29 @@ import { saveFormParam } from "../lib/common";
  * @param {D5Core} core
  */
 export const App_BTRIP_BusinessTripList = (form, core) => ({
+  OnInit() {
+    form.filterField("IsPassed").defaultValue = 0;  
+    setFieldInitialValue(form);
+  },
   OnShow() {
     saveFormParam("App_BTRIP_BusinessTripEdit", "FirmID", form.filterField("FirmID").value);
+    setFieldInitialValue(form);
   },
   OnApplyFilter() {
     saveFormParam("App_BTRIP_BusinessTripEdit", "FirmID", form.filterField("FirmID").value);
+    setFieldInitialValue(form);
+  },
+  FirmIDOnValueChanged() {
+    setFieldInitialValue(form);
   }
 });
+
+function setFieldInitialValue(form) {
+  let firmID = form.filterField("FirmID").value || -1;
+  form.filterField("PersonID").filter = {"FirmID": firmID};
+  form.filterField("ApprovedByPersonID").filter = {"FirmID": firmID};
+  form.subForm("TableExpenses").filterField("NomenclatureID").filter = {"FirmID": firmID, "NomenclatureKindID": 4};
+}
 
 window.userScript = {
   ...window.userScript,
